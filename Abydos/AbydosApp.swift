461d5f884bbcef973bef036899debfba4ee4b2fb
tokizuoh <tokizuoh@gmail.com>
2022-11-07 22:35:13 +0900

    init

---
+++ Abydos/AbydosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbydosApp.swift
+//  Abydos
+//
+//  Created by tokizo on 2022/11/07.
+//
+
+import SwiftUI
+
+@main
+struct AbydosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
