9e136a6aeef74b2d10b49aa09e158b16f5c316b4
tokizuoh <tokizuoh@gmail.com>
2022-11-13 15:40:15 +0900

    add processing get page information secret scrapbox project

--- batch/main.go
+++ batch/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	log.Println("Hello")
+	_ = godotenv.Load()
+
+	projectName := os.Getenv("SCRAPBOX_FULL_PROJECT_NAME")
+	url := fmt.Sprintf("https://scrapbox.io/api/pages/%s/", projectName)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Panicln(err)
+	}
+	log.Println(url)
+	csid := os.Getenv("CONNECT_SID")
+	cookie := http.Cookie{
+		Name:  "connect.sid",
+		Value: csid,
+	}
+	req.AddCookie(&cookie)
+	client := new(http.Client)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Panicln(resp)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Panicln(resp)
+	}
+
+	var jsonBody map[string]interface{}
+	if err = json.Unmarshal(body, &jsonBody); err != nil {
+		log.Panicln(err)
+	}
+
+	log.Println(jsonBody)
+
 }
