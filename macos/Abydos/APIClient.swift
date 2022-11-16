32cf7f3bf681da188abe816e72a64108c0323bdb
tokizuoh <tokizuoh@gmail.com>
2022-11-16 22:40:18 +0900

    モデル定義の追加

---
+++ macos/Abydos/APIClient.swift
@@ -0,0 +1,8 @@
+//
+//  APIClient.swift
+//  Abydos
+//
+//  Created by tokizo on 2022/11/16.
+//
+
+import Foundation
---
+++ macos/Abydos/PagesResponse.swift
@@ -0,0 +1,35 @@
+//
+//  PagesResponse.swift
+//  Abydos
+//
+//  Created by tokizo on 2022/11/16.
+//
+
+struct User: Codable {
+    let id: String
+}
+
+struct Page: Codable {
+    let id: String
+    let title: String
+    let image: String
+    let descriptions: [String]
+    let user: User
+    let pin: Int64
+    let views: Int
+    let linked: Int
+    let commitId: String
+    let created: Int
+    let updated: Int
+    let accessed: Int
+    let snapshotCreated: Int
+    let pageRank: Int
+}
+
+struct PagesResponse: Codable {
+    let projectName: String
+    let skip: Int
+    let limit: Int
+    let count: Int
+    let pages: [Page]
+}
