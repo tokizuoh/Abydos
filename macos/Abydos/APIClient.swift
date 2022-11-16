53fa7d7421c410beb1c5a6cd50d1169cf59a71bf
tokizuoh <tokizuoh@gmail.com>
2022-11-16 23:15:53 +0900

    add fetch processing

--- macos/Abydos/APIClient.swift
+++ macos/Abydos/APIClient.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+final class APIClient {
+    func fetch() {
+        guard let url = Dummy.url else {
+            return
+        }
+        let urlRequest = URLRequest(url: url)
+        
+        let urlSessionConfiguration = URLSessionConfiguration.default
+        urlSessionConfiguration.httpCookieAcceptPolicy = .onlyFromMainDocumentDomain
+        urlSessionConfiguration.httpShouldSetCookies = true
+        let cookie = HTTPCookie.cookies(withResponseHeaderFields: ["Set-Cookie":"\(Dummy.key)=\(Dummy.value)"], for: url)
+        urlSessionConfiguration.httpCookieStorage?.setCookie(cookie[0])
+        let task = URLSession(configuration: urlSessionConfiguration).dataTask(with: urlRequest) { data, response, error in
+            if let error {
+                print(error)
+                return
+            }
+            
+            guard let data,
+                  let response = response as? HTTPURLResponse,
+                  response.statusCode == 200 else {
+                return
+            }
+            
+            do {
+                let pagesResponse = try JSONDecoder().decode(PagesResponse.self, from: data)
+                print(pagesResponse)
+            } catch let error {
+                print(error)
+                return
+            }
+            
+            
+        }.resume()
+        
+    }
+}
--- macos/Abydos/PagesResponse.swift
+++ macos/Abydos/PagesResponse.swift
@@ -6,30 +6,30 @@
 //
 
 struct User: Codable {
-    let id: String
+    let id: String?
 }
 
 struct Page: Codable {
-    let id: String
-    let title: String
-    let image: String
+    let id: String?
+    let title: String?
+    let image: String?
     let descriptions: [String]
     let user: User
     let pin: Int64
-    let views: Int
-    let linked: Int
-    let commitId: String
-    let created: Int
-    let updated: Int
-    let accessed: Int
-    let snapshotCreated: Int
-    let pageRank: Int
+    let views: Int?
+    let linked: Int?
+    let commitId: String?
+    let created: Int?
+    let updated: Int?
+    let accessed: Int?
+    let snapshotCreated: Int?
+    let pageRank: Int?
 }
 
 struct PagesResponse: Codable {
-    let projectName: String
-    let skip: Int
-    let limit: Int
-    let count: Int
+    let projectName: String?
+    let skip: Int?
+    let limit: Int?
+    let count: Int?
     let pages: [Page]
 }
