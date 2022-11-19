ff0b22b9dde4348672c5eb4cc2f573f4b1e3aa11
tokizuoh <tokizuoh@gmail.com>
2022-11-19 12:43:36 +0900

    introduce SwiftLint

--- macos/Abydos/APIClient.swift
+++ macos/Abydos/APIClient.swift
@@ -13,34 +13,33 @@ final class APIClient {
             return
         }
         let urlRequest = URLRequest(url: url)
-        
+
         let urlSessionConfiguration = URLSessionConfiguration.default
         urlSessionConfiguration.httpCookieAcceptPolicy = .onlyFromMainDocumentDomain
         urlSessionConfiguration.httpShouldSetCookies = true
-        let cookie = HTTPCookie.cookies(withResponseHeaderFields: ["Set-Cookie":"\(Dummy.key)=\(Dummy.value)"], for: url)
+        let cookie = HTTPCookie.cookies(withResponseHeaderFields: ["Set-Cookie": "\(Dummy.key)=\(Dummy.value)"], for: url)
         urlSessionConfiguration.httpCookieStorage?.setCookie(cookie[0])
         let task = URLSession(configuration: urlSessionConfiguration).dataTask(with: urlRequest) { data, response, error in
             if let error {
                 print(error)
                 return
             }
-            
+
             guard let data,
                   let response = response as? HTTPURLResponse,
                   response.statusCode == 200 else {
                 return
             }
-            
+
             do {
                 let pagesResponse = try JSONDecoder().decode(PagesResponse.self, from: data)
                 print(pagesResponse)
-            } catch let error {
+            } catch {
                 print(error)
                 return
             }
-            
-            
+
         }.resume()
-        
+
     }
 }
