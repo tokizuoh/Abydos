5f2b6c3a22c579057e150cabf6f27541b63a5248
tokizuoh <tokizuoh@gmail.com>
2022-11-19 12:54:01 +0900

    fix swiftlint alert

--- macos/Abydos/APIClient.swift
+++ macos/Abydos/APIClient.swift
@@ -17,9 +17,12 @@ final class APIClient {
         let urlSessionConfiguration = URLSessionConfiguration.default
         urlSessionConfiguration.httpCookieAcceptPolicy = .onlyFromMainDocumentDomain
         urlSessionConfiguration.httpShouldSetCookies = true
-        let cookie = HTTPCookie.cookies(withResponseHeaderFields: ["Set-Cookie": "\(Dummy.key)=\(Dummy.value)"], for: url)
+        let cookie = HTTPCookie.cookies(
+            withResponseHeaderFields: ["Set-Cookie": "\(Dummy.key)=\(Dummy.value)"],
+            for: url
+        )
         urlSessionConfiguration.httpCookieStorage?.setCookie(cookie[0])
-        let task = URLSession(configuration: urlSessionConfiguration).dataTask(with: urlRequest) { data, response, error in
+        _ = URLSession(configuration: urlSessionConfiguration).dataTask(with: urlRequest) { data, response, error in
             if let error {
                 print(error)
                 return
