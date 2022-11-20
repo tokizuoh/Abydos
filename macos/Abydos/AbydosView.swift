33c981501581ccd2b684267b624cca0c4821161c
tokizuoh <tokizuoh@gmail.com>
2022-11-20 10:08:37 +0900

    add access modifiers

--- macos/Abydos/AbydosView.swift
+++ macos/Abydos/AbydosView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct AbydosView: View {
     @NSApplicationDelegateAdaptor(AppDelegate.self)
-    var delegate
+    private var delegate
 
     var body: some View {
         EmptyView()
